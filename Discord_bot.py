30dc1e5361224ef47a0e7291e5ba85c3a67d7d9e
SilencE3993 <108724446+SilencE3993@users.noreply.github.com>
2022-07-14 09:27:57 +0300

    Added Discord Bot

---
+++ Discord_bot.py
@@ -0,0 +1,17 @@
+import requests
+from discord.ext import commands
+from bs4 import BeautifulSoup as bs
+
+client = commands.Bot(command_prefix="/") #Создаём префикс для команд
+URL_TEMPLATE = "http://bashorg.org/byrating" #Создаём ссылку, из которой будем брать текст
+r = requests.get(URL_TEMPLATE)
+soup = bs(r.text, "html.parser")
+vacancies_names = soup.find_all('div', class_='quote') #Ищем текст по заданным критериям
+@client.command()
+async def anek(ctx): #Создаём функцию вывода текста
+    for name in vacancies_names:
+        output = str(name)[19:-6]
+        await ctx.send("\n".join(output.split("<br/>")))
+
+client.run("OTk1MzE3MzUyMjgxNDA3NTY5.GMTb_9.-UMWePDYQ4j-gSjGgVFwiIIz7zkj1cRcILV-IM") #Вставляем токен дискорд-бота
+bot.polling(none_stop=True, interval=0) #Ставим дискорд-бота на повтор
